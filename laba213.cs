259f51b5390ce8de6cd537d5b47e1e9beeabf05f
davalkaoffc <vlad8523lav@gmail.com>
2026-02-26 18:07:28 +0200

    Update and rename Program.cs to laba213.cs

--- laba213.cs
+++ laba213.cs
@@ -4,4 +4,5 @@ var результат = Console.ReadLine()
     .Where(s => !string.IsNullOrWhiteSpace(s))     
     .Select(s => s.First())                        
     .Reverse();                                  
+
 Console.WriteLine("Результат: " + string.Join(", ", результат));
