3f599f63d77870cfc83fe5e262db54bf7fcf5a01
davalkaoffc <vlad8523lav@gmail.com>
2026-02-26 18:10:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿var словник = new Dictionary<string, string[]>
+{
+    { "1", new[] { "A", "d" } },
+    { "2", new[] { "C", "B" } }
+};
+string[] масив1 = словник["1"];
+string[] масив2 = словник["2"];
+foreach (var буква1 in масив1)
+{
+    foreach (var буква2 in масив2)
+    {
+        Console.Write(буква1 + буква2);
+    }
+    Console.Write(" ");
+}
+Console.WriteLine();
