6af281e1a3f2283fb348474e02e6c23150d43b4e
davalkaoffc <vlad8523lav@gmail.com>
2026-02-26 18:08:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введіть кількість рядків: ");
+if (!int.TryParse(Console.ReadLine(), out int r) || r <= 0)
+{
+     Console.WriteLine("Помилка вводу.");
+     return;
+}
+int числа = r * 10;
+List<int> список = new List<int>(числа);
+for (int i = 1; i <= числа; i++) список.Add(i);
+for (int i = 0; i < числа; i += 10)
+{
+    bool чиЗворотнє = (i / 10) % 2 != 0;
+    for (int j = 0; j < 10; j++)
+    {
+         int index = чиЗворотнє ? i + 9 - j : i + j;
+         Console.Write(список[index] + "\t");
+    }
+    Console.WriteLine();
+}
