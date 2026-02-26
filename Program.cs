6003a6dea58f6f546e1c30350c1c948959485326
davalkaoffc <vlad8523lav@gmail.com>
2026-02-26 18:06:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введіть рядки через пробіл:");
+var результат = Console.ReadLine()
+    .Split(' ')                                
+    .Where(s => !string.IsNullOrWhiteSpace(s))     
+    .Select(s => s.First())                        
+    .Reverse();                                  
+Console.WriteLine("Результат: " + string.Join(", ", результат));
